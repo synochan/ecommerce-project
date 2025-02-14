e6e33de6a4cbbc49a9aad1730e0fd254a205e54f
Christian Dagcuta <christian.dagcuta@1.ustp.edu.ph>
2025-02-14 01:53:55 +0000

    Update

--- backend/posts/api/serializers.py
+++ backend/posts/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework.serializers import ModelSerializer
 from ..models import Post
 
 
-class PostSerializer(ModelSerializer);
+class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = ('id','title','body')
