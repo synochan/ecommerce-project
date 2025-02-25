a58687fc7764f4a005657c1d30fcdb3f8998336c
Sejayyy <143928713+zennmarieee@users.noreply.github.com>
2025-02-25 14:44:17 +0000

    Add footer component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FaFacebook, FaInstagram, FaTiktok, FaXTwitter, FaYoutube } from "react-
 function Footer() {
   return (
     <footer className="bg-gray-100 p-8 mt-10">
-      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-6 text-sm text-gray-700">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 text-sm text-gray-700">
         
         {/* Resources Section */}
         <div className="md:mb-10">
